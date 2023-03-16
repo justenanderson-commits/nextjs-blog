e2decf2bf7f699e139d12de1fb8ab3f6033aa8ce
Justen Anderson <justenanderson@gmail.com>
2023-03-16 14:59:24 -0600

    Update profile picture.

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,7 @@ export default function Home({ allPostsData }) {
         <title>A Very Interesting Title</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Here is my introduction. It is about this website.</p>
-        <p>(This is a sample website - you'll be building a site like this on {' '}<a href='https://nextjs.org/learn'>our Next.js tutorial</a>)</p>
+        <p>This website is what I build while self-teaching the Next.js framework. Check out this {' '}<a href='https://nextjs.org/learn'>Next.js tutorial if you'd like to build your own.</a></p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
